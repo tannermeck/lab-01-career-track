6e23ea5fd0c7feab02c26c0359dd525df1919253
TANNER MECK <steve.meck246@gmail.com>
2021-09-13 13:20:48 -0700

    2 tests for getName passing

--- getName.test.js
+++ getName.test.js
@@ -7,4 +7,10 @@ describe('getName returns the value of the object key name', () => {
     
     expect(name).toEqual('spot');
   });
+  it('grabs the name spot when the object is called', () => {
+    const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    const characterName = getName(character);
+    
+    expect(characterName).toEqual('Aang');
+  });
 });
