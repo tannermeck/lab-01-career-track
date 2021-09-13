158a088593815e40b8f674b0d7f2c8f25fc96751
TANNER MECK <steve.meck246@gmail.com>
2021-09-13 13:17:13 -0700

    first test passing with bracket notation

--- getName.test.js
+++ getName.test.js
@@ -1,5 +1,10 @@
+import getName from './getName.js';
+
 describe('getName returns the value of the object key name', () => {
   it('grabs the name spot when the object is called', () => {
-
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const name = getName(spot);
+    
+    expect(name).toEqual('spot');
   });
 });
